c8bf5ecf81f74c232b92982e77a93c0bcbfd04dc
manekaM <manuli.20232954@iit.ac.lk>
2025-12-02 15:03:11 +0530

    Few minor amendments

--- src/main/InvalidSurveyInputException.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class InvalidSurveyInputException extends Exception {
-    public InvalidSurveyInputException(String message) {
-        super("Survey Error: " + message);
-    }
-}
--- src/main/TeamFormationException.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class TeamFormationException extends Exception {
-    public TeamFormationException(String message) {
-        super("Team Formation Failed: " + message);
-    }
-}
